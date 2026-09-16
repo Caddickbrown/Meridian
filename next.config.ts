af38932c0ef3f5b165748d0494cfc89364490b1f
Dan Caddick-Brown <danielcaddickbrown@gmail.com>
2026-09-16 19:20:14 +0100

    fix: remove basePath, remove Eye of Horus, allow iframe from cooperativesausage

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: '/meridian',
+  
   turbopack: {
     rules: {
       'maplibre-gl.mjs': {
@@ -49,10 +49,8 @@ const nextConfig: NextConfig = {
       {
         source: '/(.*)',
         headers: [
-          { key: 'Content-Security-Policy', value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: wss: data: blob:;" },
-          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'X-Frame-Options', value: 'SAMEORIGIN' },
+          { key: 'X-Frame-Options', value: 'ALLOWALL' },
+          { key: 'Content-Security-Policy', value: "default-src 'self' 'unsafe-inline' 'unsafe-eval' https: wss: data: blob:; frame-ancestors 'self' https://cooperativesausage.com https://www.cooperativesausage.com;" },
           { key: 'X-XSS-Protection', value: '1; mode=block' },
         ],
       },
